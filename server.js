189b21a122c4124d8b2f084bb15589bbda34fbcf
Felicia Gillström <fegi2000@student.miun.se>
2022-01-16 22:24:41 +0100

    .

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
-const app = express()
+const express = require('express');
+const port = process.env.PORT || 8080;
+const app = express();
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use(express.static(__dirname + "/dist/"));
 
-const port = process.env.PORT || 8080
-app.listen(port)
+app.get(/.*/, function(req, res) {
+    res.sendFile(__dirname + "/dist/index.html");
+});
+
+app.addListener(port);
+
+console.log("Server started");
